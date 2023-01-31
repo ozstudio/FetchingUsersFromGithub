add9d8c5a0ebcf39fc98be18d0d67eebab19671f
OL <ozstudiominigames@gmail.com>
2023-01-31 16:26:47 +0200

    ini

---
+++ src/pages/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import styled from 'styled-components';
+import loginImg from '../images/login-img.svg';
+import { Link } from 'react-router-dom';
+const Login = () => {
+  return <Wrapper>
+    <img src={loginImg} alt = "user image"></img>
+    <h1>Github user</h1>
+    <Link to ="" className='btn'>login</Link>
+
+  </Wrapper>;
+};
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
+export default Login;
